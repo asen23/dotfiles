f8ce9be94133724e7a8f52d892aa4ed6c68f2785
asen23 <andersenlim23@gmail.com>
2024-07-15 12:49:13 +0700

    change exit code to 0

--- run_once_install_brew.sh
+++ run_once_install_brew.sh
@@ -3,7 +3,7 @@
 # Check if brew already installed
 if command -v brew &> /dev/null
 then
-  exit 1
+  exit 0
 fi
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
